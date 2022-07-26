aa216618c2d27f31c165476af2f71ef10faeff0b
Atjima <50250@cru.ac.th>
2022-07-26 14:44:31 +0700

    Function

---
+++ function.py
@@ -0,0 +1,11 @@
+#สร้างฟังก์ชัน
+def hello():
+    print("Hello World")
+    print("Atjima")
+#เรียกใช้งานฟังก์ชัน
+hello()  
+#ส่งค่าผ่านparameter
+def sum(x,y):
+    return "hello"
+ans = sum(8,6)/2
+print(ans)
---
+++ work-function.py
@@ -0,0 +1,10 @@
+from math import pi
+
+
+def square(width,height):
+    return width * height
+def triangle(base,height):
+    return 0.5 * base * height
+ def sq(width,height):
+    return width * height
+   
